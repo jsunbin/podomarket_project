61d76becae3db99bd1ff6400035e9a7004909a71
jsunbin <jsnb6n@gmail.com>
2026-09-11 17:46:15 +0900

    [feat] 커스텀 유저 필드 추가 및 회원가입 화면 적용

--- podomarket_project/podomarket/admin.py
+++ podomarket_project/podomarket/admin.py
@@ -4,3 +4,13 @@ from .models import User
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+
+UserAdmin.fieldsets += (
+    ("Custom Fields", {
+        "fields": (
+            "nickname",
+            "kakao_id",
+            "address",
+        ),
+    }),
+)
